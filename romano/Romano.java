9408394983a50d41ca5b370167d190201c4168e9
Lourdes Armenta (itculiacan) <69474060+LourdesArmenta@users.noreply.github.com>
2024-04-16 15:02:07 +0000

    Clase Romano

---
+++ romano/Romano.java
@@ -0,0 +1,23 @@
+// Proposito: Convertir un número arábiga del 1 al 10
+// en Romano 1-I 2-II 3-III 4-IV 5-V
+// 6-VI 7-VII 8-VIII 9-IX 10-X
+// Autor:Lourdes Armenta
+// Fecha:16/abr/24
+import java.util.Scanner;
+public class Romano {
+    public void romano(){
+        //Definir objento scanner
+        Scanner teclado = new Scanner(System.in);
+        //Definir variable de salida
+        char roma;
+        //Definir variable de entrada
+        byte ara;
+        System.out.println("Proporciona número arábigo(1-10):");
+        ara = teclado.nextByte();
+
+
+    }
+
+
+    
+}
